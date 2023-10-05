4c0923c9fddce44935d533d12712720a626d2b0b
Krishanu <krishgoswami15@gmail.com>
2023-10-05 13:20:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero'
+
+const Conatiner = styled.div
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
