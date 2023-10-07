11cea565572a9246fa0ffa1c5d57560afac8d7a3
Krishanu <krishgoswami15@gmail.com>
2023-10-07 18:23:08 +0530

    updates

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
-import Navbar from './Navbar';
 
 const Section = styled.div`
     height: 100vh;
     scroll-snap-align: center;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: column;
+    justify-content: center;
 `;
 const Container = styled.div`
     height: 100vh;
@@ -19,16 +16,15 @@ const Container = styled.div`
 `;
 
 const Left = styled.div`
+    flex: 1;
+`
+
+const Right = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     gap: 10px;
-    flex: 2;
-`
-
-const Right = styled.div`
-    flex: 3;
-    position: relative;
+    flex: 1;
 `
 const Title = styled.h1`
     font-size: 75px;
@@ -42,9 +38,8 @@ const Button = styled.button`
     cursor: pointer;
     width: 100px;
     padding: 10px;
-    background: none;
-    border-style: solid;
-    border-color: white;
+    background: #7a2d41;
+    border-style: none;
     color: white;
     border-radius: 30px;
 `
@@ -52,40 +47,19 @@ const Button = styled.button`
 const Desc = styled.p`
     font-size: 25px;
 `
-const Img = styled.img`
-    height: 500px;
-    width: 700px;
-    object-fit: contain;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-    animation: animate 2s infinite ease alternate;
-
-    @keyframes animate {
-        to{
-            transform: translateY(20px);
-        }
-    }
-`
 
 const Works = () => {
     return (
         <Section>
             <Container>
                 <Left>
+                </Left>
+                <Right>
                     <Title>Hi, my name is Krish.</Title>
                     <WhatAmI>
                         <Desc>I'm a fullstack web developer.</Desc>
                     </WhatAmI>
                     <Button>Learn More</Button>
-                </Left>
-                <Right>
-                    {/* 3dmodel */}
-                    <Img src="./images/dev.svg"></Img>
-
                 </Right>
             </Container>
         </Section>
