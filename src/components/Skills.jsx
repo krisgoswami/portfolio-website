162361fbf2a21a133379fd409a91654b915c1d08
Krishanu <krishgoswami15@gmail.com>
2023-10-08 22:31:28 +0530

    added

---
+++ src/components/Skills.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.div`
+    padding: 50px;
+    text-align: center;
+    scroll-snap-align: center;
+    align-items: center;
+    /* height: 100vh; */
+`;
+
+const Title = styled.h1`
+    font-size: 40px;
+    margin-bottom: 30px;
+    color: #eba154;
+`;
+
+const TechStackContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+`;
+
+const TechStackItem = styled.img`
+    width: 150px;
+    height: 150px;
+    margin: 20px;
+    padding: 20px;
+    background-color: #ffffff42;
+    border-radius: 20px;
+    transition: transform 0.3s; /* Added transition for smooth hover effect */
+    
+    &:hover {
+        transform: scale(1.1); /* Scale the image on hover */
+    }
+`;
+
+const Skills = () => {
+    const frontendStack = ['react', 'html', 'css', 'tailwind', 'mui', 'chakra'];
+    const backendStack = ['nodejs', 'express', 'mongodb'];
+
+    return (
+        <Section>
+            <Title>Technologies I Know</Title>
+            <div>
+                <h2>Frontend</h2>
+                <TechStackContainer>
+                    {frontendStack.map((tech, index) => (
+                        <TechStackItem key={index} src={`./images/${tech}.svg`} alt={tech} />
+                    ))}
+                </TechStackContainer>
+            </div>
+            <div>
+                <h2>Backend</h2>
+                <TechStackContainer>
+                    {backendStack.map((tech, index) => (
+                        <TechStackItem key={index} src={`./images/${tech}.svg`} alt={tech} />
+                    ))}
+                </TechStackContainer>
+            </div>
+        </Section>
+    );
+};
+
+export default Skills;
