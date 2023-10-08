31a7fe897d532150eef4c6293dc5a3b02640588d
Krishanu <krishgoswami15@gmail.com>
2023-10-08 12:31:00 +0530

    updated

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,70 +1,79 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 const Section = styled.div`
-    height: 100vh;
+    padding: 50px;
+    text-align: center;
     scroll-snap-align: center;
-    display: flex;
-    justify-content: center;
-`;
-const Container = styled.div`
     height: 100vh;
-    scroll-snap-align: center;
-    width: 1400px;
-    display: flex;
-    justify-content: space-between;
 `;
 
-const Left = styled.div`
-    flex: 1;
-`
-
-const Right = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 10px;
-    flex: 1;
-`
 const Title = styled.h1`
-    font-size: 75px;
-    padding: 0;
-    margin: 0;
-`
-const WhatAmI = styled.div`
-    
-`
-const Button = styled.button`
-    cursor: pointer;
-    width: 100px;
-    padding: 10px;
-    background: #7a2d41;
-    border-style: none;
-    color: white;
-    border-radius: 30px;
-`
+    font-size: 40px;
+    margin-bottom: 30px;
+`;
 
-const Desc = styled.p`
-    font-size: 25px;
-`
+const WorkContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 20px;
+`;
+
+const WorkCard = styled.div`
+    max-width: 400px;
+    height: 400px;
+    width: 300px;
+    background: #f9f9f933;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const WorkTitle = styled.h2`
+    font-size: 24px;
+    margin-bottom: 10px;
+`;
+
+const WorkDescription = styled.p`
+    font-size: 16px;
+`;
 
 const Works = () => {
+    const works = [
+        {
+            title: 'Website 1',
+            description: 'Description of Website 1.',
+        },
+        {
+            title: 'Website 2',
+            description: 'Description of Website 2.',
+        },
+        {
+            title: 'Website 3',
+            description: 'Description of Website 3.',
+        },
+        {
+            title: 'Website 3',
+            description: 'Description of Website 3.',
+        },
+        // Add more works as needed
+    ];
 
     return (
         <Section>
-            <Container>
-                <Left>
-                </Left>
-                <Right>
-                    <Title>Hi, my name is Krish.</Title>
-                    <WhatAmI>
-                        <Desc>I'm a fullstack web developer.</Desc>
-                    </WhatAmI>
-                    <Button>Learn More</Button>
-                </Right>
-            </Container>
+            <Title>My Works</Title>
+            <WorkContainer>
+                {works.map((work, index) => (
+                    <WorkCard key={index}>
+                        <WorkTitle>{work.title}</WorkTitle>
+                        <WorkDescription>{work.description}</WorkDescription>
+                    </WorkCard>
+                ))}
+            </WorkContainer>
         </Section>
-    )
-}
+    );
+};
 
 export default Works;
