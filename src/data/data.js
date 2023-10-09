6f59215ec135ee650ffde56d78ee749ee6dd7db4
Krishanu <krishgoswami15@gmail.com>
2023-10-09 15:43:52 +0530

    separated the data

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+export const works = [
+    {
+        title: 'Course selling app',
+        description: 'Basic course selling app made using MERN stack',
+        img: './images/website1.png',
+    },
+    {
+        title: 'Blog App',
+        description: 'Basic blog app made using MERN stack',
+        img: './images/website2.png',
+    },
+    {
+        title: 'Admin Dashboard',
+        description: 'Admin dashboard of the course app. It is used to publish courses on the main app',
+        img: './images/website3.png',
+    },
+];
+
+export const basic = [
+    { name: "HTML", img: "html" },
+    { name: "CSS", img: "css" },
+    { name: "JavaScript", img: "javascript" },
+];
+export const design = [
+    { name: "Photoshop", img: "photoshop" },
+    { name: "Illustrator", img: "illustrator" },
+];
+export const frameworks = [
+    { name: "React", img: "react" },
+    { name: "NextJS", img: "nextjs" },
+    { name: "Spring Boot", img: "spring" },
+];
+export const backend = [
+    { name: "Node", img: "nodejs" },
+    { name: "Express", img: "express" },
+    { name: "Java", img: "java" },
+];
+export const database = [
+    { name: "MongoDB", img: "mongodb" },
+    { name: "MySQL", img: "mysql" },
+    { name: "PostgreSQL", img: "postgre" },
+]
+export const libraries = [
+    { name: "MUI", img: "mui" },
+    { name: "Chakra UI", img: "chakra" },
+    { name: "Tailwind", img: "tailwind" },
+];
+export const extra = [
+    { name: "GIT", img: "git" },
+    { name: "GitHub", img: "github" },
+];
