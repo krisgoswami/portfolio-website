738eab93e8c8b43218aaeb3be2c0242cc068836b
Krishanu <krishgoswami15@gmail.com>
2023-10-10 19:46:37 +0530

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  background-color: #00000098;
+  color: #fff;
+  padding: 5px;
+  text-align: center;
+`;
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <p>&copy; 2023 Apollo The Dev. No Rights Reserved.</p>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
