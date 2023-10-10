2fec49489c0860e2f840e43bbf063d3d610193bb
Krishanu <krishgoswami15@gmail.com>
2023-10-10 12:17:35 +0530

    scroll ref added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
+import React, { useRef } from 'react';
 import './App.css'
 import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Works from './components/Works'
 import AboutMe from './components/About'
 import Skills from './components/Skills'
+import Navbar from './components/Navbar';
 
 const Conatiner = styled.div`
   height: 100vh;
@@ -19,16 +21,40 @@ const Conatiner = styled.div`
   }
 `;
 
+
 function App() {
+  const aboutMeRef = useRef(null);
+  const mySkillsRef = useRef(null);
+  const myWorksRef = useRef(null);
+  const contactMeRef = useRef(null);
+
+  const handleAboutMeClick = () => {
+    aboutMeRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const handleMySkillsClick = () => {
+    mySkillsRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const handleMyProjectsClick = () => {
+    myWorksRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const handleContactClick = () => {
+    contactMeRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <>
       <Conatiner>
+        <Navbar
+          handleAboutMeClick={handleAboutMeClick}
+          handleMySkillsClick={handleMySkillsClick}
+          handleMyProjectsClick={handleMyProjectsClick}
+          handleContactClick={handleContactClick}
+        />
         <Hero />
-        <AboutMe />
-        <Skills />
-        <Works />
-        <Contact />
+        <AboutMe ref={aboutMeRef} />
+        <Skills ref={mySkillsRef} />
+        <Works ref={myWorksRef} />
+        <Contact ref={contactMeRef} />
       </Conatiner>
     </>
   )
