8dcc70c87cbdb714ba68cf83eb76385cc8d6e2f4
Krishanu <krishgoswami15@gmail.com>
2023-10-11 17:10:48 +0530

    made changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
     return (
         <div className="h-screen flex justify-around items-center flex-col md:flex-row">
             <div className="h-full w-full flex-1 md:w-3/5 flex flex-col justify-center gap-10 md:gap-10 text-center md:text-left"> {/* Added text-center class for centering */}
-                <h1 className="text-4xl md:text-6xl font-bold leading-none md:ml-40">
+                <h1 className="text-4xl md:text-6xl font-bold leading-none md:ml-36">
                     Hi, my name is <span className="text-orange-400">Krish</span>.
                 </h1>
-                <p className="text-xl sm:text-xl md:text-2xl mb-6 md:ml-40">I'm a budding fullstack web developer.</p>
-                <div className="flex gap-4 mx-auto md:ml-40 ">
+                <p className="text-xl sm:text-xl md:text-2xl mb-6 md:ml-36">I'm a budding fullstack web developer.</p>
+                <div className="flex gap-4 mx-auto md:ml-36">
                     {socials.map((site, index) =>
                         <a href={site.href} target='_blank' rel="noopener noreferrer" key={index}>
                             <img src={`./images/${site.name}.svg`} alt={site.name} className="h-8" />
