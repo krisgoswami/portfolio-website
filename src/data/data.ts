7168a83950f47f5b037a44048391ae86816811dc
Krishanu <krishgoswami15@gmail.com>
2024-01-03 20:58:01 +0530

    changed to ts

---
+++ src/data/data.ts
@@ -0,0 +1,108 @@
+export const socials = [
+    {
+        name: "linkedin",
+        href: "https://www.linkedin.com/in/krishanu-goswami/",
+    },
+    {
+        name: "githubwhite",
+        href: "https://github.com/krisgoswami",
+    },
+]
+
+export const works = [
+    {
+        title: 'Course selling app',
+        description: 'Basic course selling app made using MERN stack',
+        img: './images/website1.png',
+        href: 'https://coursez-by-krish.netlify.app/',
+    },
+    {
+        title: 'Blog App',
+        description: 'Basic blog app made using MERN stack',
+        img: './images/website2.png',
+        href: 'https://mernblogapp-by-krish.netlify.app/',
+    },
+    {
+        title: 'Online Marketplace app',
+        description: 'A marketplace app, where user can buy and sell stuff.',
+        img: './images/website3.png',
+        href: 'https://github.com/krisgoswami/online-marketplace.git',
+    },
+    {
+        title: 'Ground Booking app',
+        description: 'A ground booking app for sports lovers.',
+        img: './images/website4.png',
+        href: 'https://github.com/krisgoswami/ground-booking-app.git',
+    },
+    {
+        title: 'Course App',
+        description: 'Course app with dark/light mode UI.',
+        img: './images/website5.png',
+        href: 'https://github.com/krisgoswami/alemeno-courses.git',
+    },
+];
+
+export const workex = [
+    {
+        company: "Kinetiq",
+        position: "Team Lead (Advertisement Data Analyst and QA)",
+        duration: "Sep 2020 - May 2022"
+    },
+    {
+        company: "Medusind",
+        position: "Senior Client Service Executive",
+        duration: "May 2018 - Jun 2019"
+    },
+]
+
+export const education = [
+    {
+        degree: "MS in Computer Science",
+        university: "Liverpool John Moores University",
+        completion: "Sep 2022 - Aug 2023"
+    },
+    {
+        degree: "Post Graduation Diploma",
+        university: "IIIT-Bangalore",
+        completion: "Aug 2021 - Sep 2022"
+    },
+    {
+        degree: "BCA",
+        university: "Navgujarat College of Computer Applications",
+        completion: "Jun 2013 - May 2016"
+    },
+]
+
+export const basic = [
+    { name: "HTML", img: "html" },
+    { name: "CSS", img: "css" },
+    { name: "JavaScript", img: "javascript" },
+];
+export const design = [
+    { name: "Photoshop", img: "photoshop" },
+    { name: "Illustrator", img: "illustrator" },
+];
+export const frameworks = [
+    { name: "React", img: "react" },
+    { name: "NextJS", img: "nextjs" },
+    { name: "Spring Boot", img: "spring" },
+];
+export const backend = [
+    { name: "Node", img: "nodejs" },
+    { name: "Express", img: "express" },
+    { name: "Java", img: "java" },
+];
+export const database = [
+    { name: "MongoDB", img: "mongodb" },
+    { name: "MySQL", img: "mysql" },
+    { name: "PostgreSQL", img: "postgre" },
+]
+export const libraries = [
+    { name: "MUI", img: "mui" },
+    { name: "Chakra UI", img: "chakra" },
+    { name: "Tailwind", img: "tailwind" },
+];
+export const extra = [
+    { name: "GIT", img: "git" },
+    { name: "GitHub", img: "github" },
+];
